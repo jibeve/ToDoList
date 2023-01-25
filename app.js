4fa40b8cbedaa426da7416e988d1eb7987e8d445
jibeve <jeanbaptiste.voillemin@gmail.com>
2023-01-25 06:13:46 +0100

    Clean code

--- app.js
+++ app.js
@@ -1,33 +1,51 @@
 const express = require('express');
 const parser = require('body-parser');
 const PORT = process.env.PORT || 3000;
+const date = require(__dirname + "/date.js") //as the module is local (and not install via npm) __dirname + "/date.js"
 
 const app = express();
 
-var items = ["Ajouter des items"];
+let items = ["Ajouter des items"];
+let workItems = [];
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(parser.urlencoded({extended: true}));
 
 app.get('/', (req, res) => {
-    var today = new Date();
-    var options = {
-        weekday: "long",
-        day: "numeric",
-        month: "long",
-    };
+    let day = date.getDate();
 
-    var day = today.toLocaleDateString("fr-FR", options);
-    res.render('list', {kindOfDay: day, newListItems: items});
+    res.render('list', {listTitle: day, newListItems: items});
+})
+
+app.get('/work', (req, res)=>{
+    res.render('list', {listTitle: "work list", newListItems: workItems});
+})
+
+app.get('/about', (req, res)=>{
+    res.render('about');
 })
 
 app.post('/', (req, res)=> {
-    var item = req.body.newItem;
-    items.push(item);
-    //console.log(item);
-    res.redirect('/');
+    let item = req.body.newItem;
+    console.log(req.body);
+    if (req.body.list === "work"){
+        workItems.push(item);
+        res.redirect('/work');
+    } else {
+        items.push(item);
+        res.redirect('/');}
+
+    // items.push(item);
+    // //console.log(item);
+    // res.redirect('/');
 })
+
+// app.post('/work', (req, res) => {
+//     let item = req.body.newItem;
+//     workItems.push(item);
+//     res.redirect('/work');
+// })
 
 app.listen(PORT, () => {
     console.log(`Server is up and running on port ${PORT}`)
